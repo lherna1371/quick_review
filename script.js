515d798b6bdaed9fa41737fdc5c36a9c674bfc82
lherna1371 <lherna1371@gmail.com>
2022-05-02 20:41:15 -0500

    a little review for you - second commit!

--- script.js
+++ script.js
@@ -2,20 +2,42 @@
 // 1
 // create a button and append it to the card element 
 
+let button = document.createElement('button');
+button.textContent = 'click me!'
+
+let card = document.querySelector('.card')
+card.append(button)
+
+
 // 2
 // inlude a class (title) to the p tag for job/role title 
 
 // 3
 // Select all the link and set them to target _blank
 
+let aTags = document.querySelectorAll('a');
+
+for(let i = 0; i < aTags.length; i++){
+    aTags[i].setAttribute('target', '_blank')
+}
+
+
 // 4
-// create a function, call it updateCard
-
-    // function should change the header (h2) for education and set to blue
-
-    // change card background red 
-
-    // remove the education header and list from the card
-
-// 3
 // add an event listener to the button and on click call updateCard function
+
+
+button.addEventListener('click', function(e){
+    console.log(e)
+
+    // select and update h2 text 
+    let educationHeader = document.querySelector('.education-h');
+    educationHeader.style.color = 'red';
+
+
+    // udpate card color
+    card.style.backgroundColor = 'red';
+
+    // remove ul to remove li items 
+    let ulItem = document.querySelector('ul')
+    ulItem.remove()
+})
