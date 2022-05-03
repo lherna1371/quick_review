c3b0cd661883bfdfea27b8591938112fd6aa8995
lherna1371 <lherna1371@gmail.com>
2022-05-02 20:01:49 -0500

    a little review for you!

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// 1
+// create a button and append it to the card element 
+
+// 2
+// inlude a class (title) to the p tag for job/role title 
+
+// 3
+// Select all the link and set them to target _blank
+
+// 4
+// create a function, call it updateCard
+
+    // function should change the header (h2) for education and set to blue
+
+    // change card background red 
+
+    // remove the education header and list from the card
+
+// 3
+// add an event listener to the button and on click call updateCard function
